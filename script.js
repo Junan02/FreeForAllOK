9aecad6cda16c55481641934bdb8d22d49efbfb1
Junan02 <87476149+Junan02@users.noreply.github.com>
2024-08-13 18:12:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+//Prevent right click
+document.oncountextmenu = () => {
+  alert(HAHA)
+  return false
+}
 
+//Prevent Keys
+
+document.onkeydown = e => {
+  if(e.key == "F12") {
+    alert ("Don't try to inspect element")
+    return false
+  }
+
+  //prevent ctrl + u
+   if(e.ctrlkey && e.key == "u") {
+    alert ("lol")
+    return false
+  }
+
+  //prevent copy
+  if(e.ctrlkey && e.key == "c") {
+    alert ("No No")
+    return false
+  }
+
+    //prevent Paste
+  if(e.ctrlkey && e.key == "v") {
+    alert ("Bruh")
+    return false
+  }
